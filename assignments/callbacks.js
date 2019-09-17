afa568911abef6c10a74297d300cc37fe76b2e8f
[Brandon Teague] <[teaguebrandon@outlook.com]>
2019-09-17 19:23:21 -0400

    finished

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,6 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
 
 /* 
 
@@ -38,13 +38,20 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   console.log(test2); // "this Pencil is worth a million dollars!"
 */
 
-
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr);
 }
+
+const arrLength = function(arr) {
+  return arr.length;
+};
+
+console.log(arrLength(items));
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr.length - 1);
 }
 
 function sumNums(x, y, cb) {
@@ -53,12 +60,32 @@ function sumNums(x, y, cb) {
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x, y);
 }
+
+const multiply = function(x, y) {
+  return x * y;
+};
+
+console.log(multiplyNums(3, 3, multiply));
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(item, list);
 }
+
+const check = function(item, list) {
+  for (i = 0; i < list.length; i++) {
+    if (list[i] === item) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
+
+console.log(contains("Pencil", items, check));
 
 /* STRETCH PROBLEM */
 
